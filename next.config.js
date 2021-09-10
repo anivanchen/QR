8332fa674d92453234234da4611a732970e82606
Ivan Chen <ivanchen07@gmail.com>
2021-09-09 21:43:23 -0400

    Revert to d69453a

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const debug = process.env.NODE_ENV !== "production";
-
 module.exports = {
   reactStrictMode: true,
-  exportPathMap: function () { // /Next-React-Components
-    return {
-      "/qr": { page: "/qr" },
-    }
-  },
-  assetPrefix: !debug ? 'https://anivanchen.github.io/qr' : '',
 }
